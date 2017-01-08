b2090f24ba5c02c8d9b6e828db4a0eb82a1c58f9
arve0 <arve.seljebu@gmail.com>
2017-01-08 17:52:26 +0100

    build with babel

--- build.js
+++ build.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
+var babel = require('babel-core');
 
 var html = fs.readFileSync('_index.html', 'utf8');
 var style = fs.readFileSync('style.css', 'utf8');
 var script = fs.readFileSync('index.js', 'utf8');
+
+script = babel.transform(script, { presets: ["env"] }).code;
 
 html = html.replace('<style></style>', `<style>${style}</style>`);
 html = html.replace('<script></script>', `<script>${script}</script>`);
